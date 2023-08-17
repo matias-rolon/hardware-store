86ca19d14bbb5e012748241ce8229eeb0919bb63
Matias Rolón <matiasrolon1301@gmail.com>
2023-08-17 15:29:13 -0300

    Refactoriacion del Navbar

---
+++ src/components/Navbar/NavDropdown.tsx
@@ -0,0 +1,108 @@
+
+import '../styles/dropdown.css'
+
+
+export const NavDropdown = () => {
+
+
+    return (
+        <>
+            <li className="nav-item dropdown">
+                <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    Dropdown
+                </a>
+                <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <ul>
+                        <h6 className="dropdown-header">Indumentaria</h6>
+                        <li>
+                            <a className="dropdown-item" href="#">Mega Menu Link</a>
+                        </li>
+                        <li>
+                            <a className="dropdown-item" href="#">Mega Menu Link</a>
+                        </li>
+                        <li>
+                            <a className="dropdown-item" href="#">Mega Menu Link</a>
+                        </li>
+                        <li>
+                            <a className="dropdown-item" href="#">Mega Menu Link</a>
+                        </li>
+                    </ul>
+                    <ul>
+                        <h6 className="dropdown-header">Indumentaria</h6>
+                        <li>
+                            <a className="dropdown-item" href="#">Mega Menu Link</a>
+                        </li>
+                        <li>
+                            <a className="dropdown-item" href="#">Mega Menu Link</a>
+                        </li>
+                        <li>
+                            <a className="dropdown-item" href="#">Mega Menu Link</a>
+                        </li>
+                    </ul>
+                    <ul>
+                        <h6 className="dropdown-header">Indumentaria</h6>
+                        <li>
+                            <a className="dropdown-item" href="#">Mega Menu Link</a>
+                        </li>
+                        <li>
+                            <a className="dropdown-item" href="#">Mega Menu Link</a>
+                        </li>
+                        <li>
+                            <a className="dropdown-item" href="#">Mega Menu Link</a>
+                        </li>
+                        <li>
+                            <a className="dropdown-item" href="#">Mega Menu Link</a>
+                        </li>
+                    </ul>
+                    <ul>
+                        <h6 className="dropdown-header">Indumentaria</h6>
+                        <li>
+                            <a className="dropdown-item" href="#">Mega Menu Link</a>
+                        </li>
+                        <li>
+                            <a className="dropdown-item" href="#">Mega Menu Link</a>
+                        </li>
+                        <li>
+                            <a className="dropdown-item" href="#">Mega Menu Link</a>
+                        </li>
+                        <li>
+                            <a className="dropdown-item" href="#">Mega Menu Link</a>
+                        </li>
+                    </ul>
+                    <ul>
+                        <h6 className="dropdown-header">Indumentaria</h6>
+                        <li>
+                            <a className="dropdown-item" href="#">Mega Menu Link</a>
+                        </li>
+                        <li>
+                            <a className="dropdown-item" href="#">Mega Menu Link</a>
+                        </li>
+                        <li>
+                            <a className="dropdown-item" href="#">Mega Menu Link</a>
+                        </li>
+                        <li>
+                            <a className="dropdown-item" href="#">Mega Menu Link</a>
+                        </li>
+                    </ul>
+                    <ul>
+                        <h6 className="dropdown-header">Indumentaria</h6>
+                        <li>
+                            <a className="dropdown-item" href="#">Mega Menu Link</a>
+                        </li>
+                        <li>
+                            <a className="dropdown-item" href="#">Mega Menu Link</a>
+                        </li>
+                        <li>
+                            <a className="dropdown-item" href="#">Mega Menu Link</a>
+                        </li>
+                        <li>
+                            <a className="dropdown-item" href="#">Mega Menu Link</a>
+                        </li>
+                    </ul>
+                </div>
+            </li>
+        </>
+    )
+
+
+}
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,95 +1,29 @@
-import SearchIcon from '@mui/icons-material/Search';
-
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 
 import '../styles/navbar.css'
-
+import {NavItems} from './NavItems';
+import {NavDropdown} from './NavDropdown';
+import {SearchNav} from './SearchNav';
 
 
 export const Navbar = () => {
 
     return (
-        <>
-            <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                <div className="container-fluid">
-                    <a className="navbar-brand" href="#">Mi Tienda</a>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <div className="collapse navbar-collapse" id="navbarNav">
-                        <ul className="navbar-nav">
-                            <li className="nav-item">
-                                <a className="nav-link" href="#">Inicio</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="#">Contacto</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="#">Sobre nosotros</a>
-                            </li>
-                            <li className="nav-item dropdown">
-                                <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                    Productos
-                                </a>
-                                <ul className="dropdown-menu multi-column columns-2" aria-labelledby="navbarDropdown" id='listas'>
-                                    <div className='row'>
-
-                                        <div className="col-md-6">
-                                            <li>
-                                                <h6 className="dropdown-header">Indumentaria</h6>
-                                                <a className="dropdown-item" href="#">Guantes</a>
-                                                <a className="dropdown-item" href="#">Casco</a>
-                                                <a className="dropdown-item" href="#">Zapatillas</a>
-                                                <a className="dropdown-item" href="#">Delantal</a>
-                                            </li>
-
-                                        </div>
-                                        <div className="col-md-6">
-                                            <li>
-                                                <h6 className="dropdown-header">Herramientas</h6>
-                                                <a className="dropdown-item" href="#">Martillo</a>
-                                                <a className="dropdown-item" href="#">Destornillador</a>
-                                                <a className="dropdown-item" href="#">Tester</a>
-                                                <a className="dropdown-item" href="#">Pinzas</a>
-                                            </li>
-
-                                        </div>
-                                        <div className="col-md-6">
-                                            <li>
-                                                <h6 className="dropdown-header">Iluminación</h6>
-                                                <a className="dropdown-item" href="#">Luces led</a>
-                                                <a className="dropdown-item" href="#">Reflectores</a>
-                                                <a className="dropdown-item" href="#">Instalacion</a>
-                                            </li>
-
-                                        </div>
-                                        <div className="col-md-6">
-                                            <li>
-                                                <h6 className="dropdown-header">Maquinas electricas</h6>
-                                                <a className="dropdown-item" href="#">Amoladora</a>
-                                                <a className="dropdown-item" href="#">Caladora</a>
-                                                <a className="dropdown-item" href="#">Lijadora</a>
-                                                <a className="dropdown-item" href="#">Sierra circular</a>
-                                                <a className="dropdown-item" href="#">Destornilladora electrica</a>
-                                            </li>
-
-                                        </div>
-
-                                    </div>
-                                </ul>
-                            </li>
-                        </ul>
-                        <form className="d-flex ms-auto">
-                            <input className="search" placeholder="Buscar productos"/>
-                            <button className="search-button" type="submit">
-                                <SearchIcon/>
-                            </button>
-                        </form>
-                    </div>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <div className="container-fluid">
+                <a className="navbar-brand" href="#">Mi Tienda</a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                        <NavItems/>
+                        <NavDropdown/>
+                    </ul>
+                    <SearchNav/>
                 </div>
-            </nav>
-
-        </>
+            </div>
+        </nav>
     )
 }
