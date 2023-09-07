22d781edacf1a80c8f605361ef23c242563bcecf
Matias Rolón <matiasrolon1301@gmail.com>
2023-09-07 14:00:46 -0300

    Nuevo estilos del navbar

--- src/components/Navbar/NavDropdown.tsx
+++ src/components/Navbar/NavDropdown.tsx
@@ -1,105 +1,50 @@
 
 import '../styles/dropdown.css'
 
+interface Props {
+    name: string
+    dropdown?: Items[];
+}
 
-export const NavDropdown = () => {
+interface Items {
+    nameSection: string;
+    itemSection: Section[]
+}
+
+interface Section {
+    nameItem: string;
+    url: string;
+}
+
+export const NavDropdown = ({name, dropdown}: Props) => {
 
 
     return (
         <>
             <li className="nav-item dropdown">
-                <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                    Dropdown
+                <a  className={`nav-link ${dropdown ? 'dropdown-toggle' : ''}`} href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    {name}
                 </a>
-                <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-                    <ul>
-                        <h6 className="dropdown-header">Indumentaria</h6>
-                        <li>
-                            <a className="dropdown-item" href="#">Gauantes</a>
-                        </li>
-                        <li>
-                            <a className="dropdown-item" href="#">Delantales</a>
-                        </li>
-                        <li>
-                            <a className="dropdown-item" href="#">Cascos</a>
-                        </li>
-                        <li>
-                            <a className="dropdown-item" href="#">Protecciones de seguridad</a>
-                        </li>
-                    </ul>
-                    <ul>
-                        <h6 className="dropdown-header">Indumentaria</h6>
-                        <li>
-                            <a className="dropdown-item" href="#">Mega Menu Link</a>
-                        </li>
-                        <li>
-                            <a className="dropdown-item" href="#">Mega Menu Link</a>
-                        </li>
-                        <li>
-                            <a className="dropdown-item" href="#">Mega Menu Link</a>
-                        </li>
-                    </ul>
-                    <ul>
-                        <h6 className="dropdown-header">Indumentaria</h6>
-                        <li>
-                            <a className="dropdown-item" href="#">Mega Menu Link</a>
-                        </li>
-                        <li>
-                            <a className="dropdown-item" href="#">Mega Menu Link</a>
-                        </li>
-                        <li>
-                            <a className="dropdown-item" href="#">Mega Menu Link</a>
-                        </li>
-                        <li>
-                            <a className="dropdown-item" href="#">Mega Menu Link</a>
-                        </li>
-                    </ul>
-                    <ul>
-                        <h6 className="dropdown-header">Indumentaria</h6>
-                        <li>
-                            <a className="dropdown-item" href="#">Mega Menu Link</a>
-                        </li>
-                        <li>
-                            <a className="dropdown-item" href="#">Mega Menu Link</a>
-                        </li>
-                        <li>
-                            <a className="dropdown-item" href="#">Mega Menu Link</a>
-                        </li>
-                        <li>
-                            <a className="dropdown-item" href="#">Mega Menu Link</a>
-                        </li>
-                    </ul>
-                    <ul>
-                        <h6 className="dropdown-header">Indumentaria</h6>
-                        <li>
-                            <a className="dropdown-item" href="#">Mega Menu Link</a>
-                        </li>
-                        <li>
-                            <a className="dropdown-item" href="#">Mega Menu Link</a>
-                        </li>
-                        <li>
-                            <a className="dropdown-item" href="#">Mega Menu Link</a>
-                        </li>
-                        <li>
-                            <a className="dropdown-item" href="#">Mega Menu Link</a>
-                        </li>
-                    </ul>
-                    <ul>
-                        <h6 className="dropdown-header">Indumentaria</h6>
-                        <li>
-                            <a className="dropdown-item" href="#">Mega Menu Link</a>
-                        </li>
-                        <li>
-                            <a className="dropdown-item" href="#">Mega Menu Link</a>
-                        </li>
-                        <li>
-                            <a className="dropdown-item" href="#">Mega Menu Link</a>
-                        </li>
-                        <li>
-                            <a className="dropdown-item" href="#">Mega Menu Link</a>
-                        </li>
-                    </ul>
-                </div>
+                {
+                    dropdown ? 
+                        <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+                        {
+                            dropdown.map( dropdown => (
+                                <ul>
+                                    <h6 className="dropdown-header">{dropdown.nameSection}</h6>
+                                        {
+                                            dropdown.itemSection.map( item => (
+                                                <li>
+                                                    <a className="dropdown-item" href={item.url}>{item.nameItem}</a>
+                                                </li>
+                                            ) )
+                                        }
+                                </ul>
+                            ) )
+                        }
+                        </div>
+                : null} 
+                    
             </li>
         </>
     )
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -5,7 +5,7 @@ import '../styles/navbar.css';
 
 import { useNavigate } from 'react-router-dom';
 import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
-import QuestionAnswerOutlinedIcon from '@mui/icons-material/QuestionAnswerOutlined';
+import BookmarkBorderIcon from '@mui/icons-material/BookmarkBorder';
 import {NavDropdown} from './NavDropdown';
 import {SearchNav} from './SearchNav';
 
@@ -16,36 +16,33 @@ export const Navbar = () => {
 
     return (
         <>
-            <nav className="navbar navbar-expand-lg navbar-dark">
+            <nav className="navbar navbar-expand-lg">
                 <div className="container-fluid">
                     <div className="navbar-brand-container" onClick={ () => navigation('/') }>
-                        <a className="navbar-brand logo" href="#">AMK</a>
-                        <a className="navbar-brand subtext with-line" href="#">S.R.L</a>
+                        <a className="navbar-brand logo" href="#">AMK SRL</a>
+                        <a className="navbar-brand subtext with-line" href="#">FERRETERIA Y SEGURIDAD INDUSTRIAL</a>
                     </div>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                            <NavDropdown/>
-                            <NavDropdown/>
-                            <NavDropdown/>
-                            <NavDropdown/>
-                            <NavDropdown/>
-
+                            <NavDropdown name='Home'/>
+                            <NavDropdown name='Contactanos'/>
+                            <NavDropdown name='Variable_def_1' dropdown={[]}/>
+                            <NavDropdown name='Variable_def_2' dropdown={[]}/>
                         </ul>
                         <SearchNav/>
                         <ShoppingCartOutlinedIcon className='icon-shop'/>
-                        <QuestionAnswerOutlinedIcon className='icon-help' />
+                        <BookmarkBorderIcon className='icon-help' />
                     </div>
                 </div>
             </nav>
             <div className="sections">
-                <NavDropdown/>
-                <NavDropdown/>
-                <NavDropdown/>
-                <NavDropdown/>
-                <NavDropdown/>
+                <NavDropdown name='Home'/>
+                <NavDropdown name='Contactanos'/>
+                <NavDropdown name='Variable_def_1' dropdown={[]}/>
+                <NavDropdown name='Variable_def_2' dropdown={[]}/>
             </div>
         </>
     )
